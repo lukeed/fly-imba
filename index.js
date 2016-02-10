c32c9aafb473c250871398feaf17a4b22ee96a1c
luke <luke.edwards05@gmail.com>
2016-02-10 10:28:42 -0800

    working filter plugin!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const imba = require('imba')
 
 module.exports = function () {
-  this.filter('imba', function() {
-    return imba.compile()
+  this.filter('imba', function(data) {
+    return {code: imba.compile(data.toString()), ext: '.js'}
   });
 };
