55a14009dd1c308d410a8a418413af4ff2bc8ee4
luke <luke.edwards05@gmail.com>
2016-02-10 10:28:49 -0800

    create tests & test fixtures

---
+++ test/a.imba
@@ -0,0 +1,8 @@
+# Define a simple class for our data objects
+class Test
+  prop title
+  prop completed
+
+  def initialize title, completed = no
+    @title = title
+    @completed = completed
--- test/test.js
+++ test/test.js
@@ -1,5 +1,20 @@
-const test = require("tape").test
+const fs = require('fs')
+const test = require('tape').test
+const imba = require('../')
 
-test("fly-imba", (t) => {
-  t.end()
+test('fly-imba', (t) => {
+  t.plan(3)
+
+  const file1 = fs.readFileSync('test/a.imba')
+  const file2 = fs.readFileSync('test/a.js', 'utf8')
+
+  imba.call({
+    filter: function(name, fn) {
+      const results = fn(file1)
+
+      t.equal(name, 'imba', 'add imba filter to fly')
+      t.equal(results.ext, '.js', 'compiled to ".js" file')
+      t.equal(results.code.js, file2, 'compiled to javascript')
+    }
+  })
 })
