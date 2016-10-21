ab36e98ddaa288c0a71f05baa985f1d8511a45a9
Luke Edwards <luke.edwards05@gmail.com>
2016-10-21 10:35:51 +0200

    upgrade plugin logic

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const imba = require('imba/compiler')
+'use strict';
+
+const imba = require('imba/compiler');
 
 module.exports = function () {
-  this.filter('imba', function(data) {
-    return {code: imba.compile(data.toString()), ext: '.js'}
-  });
+	this.plugin('imba', {}, function * (file, opts) {
+		opts = Object.assign({bare: false, sourceMap: false}, opts);
+
+		// update file type
+		file.base = file.base.replace('.imba', '.js');
+
+		// compile data
+		const out = imba.compile(file.data.toString());
+
+		// set data
+		file.data = new Buffer(out);
+	});
 };
