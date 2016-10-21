29584819bc6bea6aa7d36ae8810adf2956e48371
Luke Edwards <luke.edwards05@gmail.com>
2016-10-21 11:59:24 +0200

    add sourcemap (& inline) support

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 'use strict';
 
+const format = require('path').format;
 const imba = require('imba/compiler');
 
 module.exports = function () {
 	this.plugin('imba', {}, function * (file, opts) {
 		opts = Object.assign({bare: false, sourceMap: false}, opts);
 
+		// modify options for source mapping
+		if (opts.sourceMap) {
+			opts.sourcePath = format(file);
+		}
+
 		// update file type
 		file.base = file.base.replace('.imba', '.js');
 
 		// compile data
-		const out = imba.compile(file.data.toString());
+		const out = imba.compile(file.data.toString(), opts);
+
+		// if had sourcemap content
+		if (out.sourcemap) {
+			let add;
+			const map = new Buffer(JSON.stringify(out.sourcemap));
+
+			// if was `inline`
+			if (opts.sourceMapInline) {
+				add = `data:application/json;base64,${map.toString('base64')}`;
+			} else {
+				add = file.base.concat('.map');
+				// add to `fly._.files`
+				this._.files.push({
+					base: add,
+					dir: file.dir,
+					data: map
+				});
+			}
+
+			// add sourcemap reference to output
+			out.js += `\n//# sourceMappingURL=${add}`;
+		}
 
 		// set data
-		file.data = new Buffer(out);
+		file.data = new Buffer(out.js);
 	});
 };
