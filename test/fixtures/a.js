40774e5d7b8291486ec1d06bea4ab1a311aa5bdf
Luke Edwards <luke.edwards05@gmail.com>
2016-10-21 10:24:05 +0200

    move test files

---
+++ test/fixtures/a.js
@@ -0,0 +1,15 @@
+(function(){
+  // Define a simple class for our data objects
+  function Test(title,completed){
+    if(completed === undefined) completed = false;
+    this._title = title;
+    this._completed = completed;
+  };
+  Test.prototype.title = function(v){ return this._title; }
+  Test.prototype.setTitle = function(v){ this._title = v; return this; };
+  Test.prototype.completed = function(v){ return this._completed; }
+  Test.prototype.setCompleted = function(v){ this._completed = v; return this; };
+
+  return Test;
+
+})()
